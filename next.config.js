6a2bc5ef972fbfb810bc6e4943cb7a6bfb093f47
Leonardo Galante <lndgalante@gmail.com>
2022-11-05 14:22:12 +0000

    Ignore build errors on next build

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
